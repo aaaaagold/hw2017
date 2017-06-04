228fad5ef1b7f7ebab45f435e98f550d25443f20
aaaaagold <alexhhxela@gmail.com>
2017-06-04 13:53:31 +0800

    Update main.cpp

--- ai2017/hw2/main.cpp
+++ ai2017/hw2/main.cpp
@@ -19,18 +19,26 @@ using namespace std;
 
 int main(const int argc,const char *argv[])
 {
+	const string arg_qd="-qd"; // no print distinguish
+	const string arg_qn="-qn"; // no print nb info
+	bool opt_qd=0;
+	bool opt_qn=0;
+	for(int x=1;x<argc;x++)
+	{
+		if(arg_qd==argv[x]) opt_qd=1;
+		if(arg_qn==argv[x]) opt_qn=1;
+	}
+	
 	alldata tmp;
 	tmp.readAlldata(argv[1]);
-	bool printClass=argc>2;
-	tmp.print();
 	dataFormat &h=tmp.head;
 	nb xd(tmp.rv,h);
 	size_t errCnt=0;
 	map<string,size_t> errCntEach; for(size_t x=h.o.getinfo().size();x--;) errCntEach[h.o.getinfo()[x] ]=0;
 	for(int x=0,xs=tmp.rv.size();x<xs;x++)
 	{
-		string c=xd.distinguish(tmp.rv[x],printClass);
-		cout<<tmp.rv[x].output()<<" -> "<<c<<endl<<endl;
+		string c=xd.distinguish(tmp.rv[x],!opt_qd);
+		if(!opt_qd) cout<<tmp.rv[x].output()<<" -> "<<c<<endl<<endl;
 		if(tmp.rv[x].output()!=c)
 		{
 			errCnt++;
@@ -38,10 +46,13 @@ int main(const int argc,const char *argv[])
 		}
 	}
 	cout<<"error:"<<errCnt<<" total:"<<tmp.rv.size()<<endl;
-	cout<<"error in each class: ";
-		for(auto it=errCntEach.begin();it!=errCntEach.end();it++) cout<<"\t"<<it->first<<":"<<it->second;
+	cout<<"error in each class:\t";
+		for(auto it=errCntEach.begin();it!=errCntEach.end();it++) cout<<" "<<it->first<<":"<<it->second;
 	cout<<endl;
-	xd.printcp();
-	xd.printfs();
+	if(!opt_qn)
+	{
+		xd.printcp();
+		xd.printfs();
+	}
 	return 0;
 }
