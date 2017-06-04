90b50d8b3c72141e70465cf4b43e47c63d51f55f
aaaaagold <alexhhxela@gmail.com>
2017-06-04 13:19:30 +0800

    Update main.cpp

--- ai2017/hw2/main.cpp
+++ ai2017/hw2/main.cpp
@@ -23,12 +23,24 @@ int main(const int argc,const char *argv[])
 	tmp.readAlldata(argv[1]);
 	bool printClass=argc>2;
 	tmp.print();
-	nb xd(tmp.rv,tmp.head);
+	dataFormat &h=tmp.head;
+	nb xd(tmp.rv,h);
+	size_t errCnt=0;
+	map<string,size_t> errCntEach; for(size_t x=h.o.getinfo().size();x--;) errCntEach[h.o.getinfo()[x] ]=0;
 	for(int x=0,xs=tmp.rv.size();x<xs;x++)
 	{
 		string c=xd.distinguish(tmp.rv[x],printClass);
 		cout<<tmp.rv[x].output()<<" -> "<<c<<endl<<endl;
+		if(tmp.rv[x].output()!=c)
+		{
+			errCnt++;
+			errCntEach[tmp.rv[x].output()]++;
+		}
 	}
+	cout<<"error:"<<errCnt<<" total:"<<tmp.rv.size()<<endl;
+	cout<<"error in each class: ";
+		for(auto it=errCntEach.begin();it!=errCntEach.end();it++) cout<<"\t"<<it->first<<":"<<it->second;
+	cout<<endl;
 	xd.printcp();
 	xd.printfs();
 	return 0;
