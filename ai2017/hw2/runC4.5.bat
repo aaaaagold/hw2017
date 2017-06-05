fd438eb284dd1b4fa10ab3ca49cc4f962366b906
aaaaagold <alexhhxela@gmail.com>
2017-06-05 11:13:58 +0800

    Update runC4.5.bat

--- ai2017/hw2/runC4.5.bat
+++ ai2017/hw2/runC4.5.bat
@@ -2,14 +2,6 @@
 @set C4_5Path="Windows Version\C4.5\bin\Debug\C4.exe"
 @set datafold="dataset"
 
-@echo cutting data
-@set theexe=cv-main.cpp.exe
-@%theexe% %datafold%\adult
-@%theexe% %datafold%\car
-@%theexe% %datafold%\isolet
-@%theexe% %datafold%\page-blocks
-@%theexe% %datafold%\winequality
-
 @set theexe=%C4_5Path%
 @for /L %%i in (1,1,10) do @(
 @echo fold %%i
