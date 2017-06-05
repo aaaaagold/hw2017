b1c6d271e2c922f4d3e2af7e36ea77b0971b0ede
aaaaagold <alexhhxela@gmail.com>
2017-06-05 12:24:02 +0800

    Update runNB.bat

--- ai2017/hw2/runNB.bat
+++ ai2017/hw2/runNB.bat
@@ -1,15 +1,42 @@
 
 @set NBPath=nb-main.cpp.exe
-@set datafold="dataset"
+@set datafolder="dataset"
 
 @set theexe=%NBPath%
+
+@set data=%datafolder%\adult
+@echo %data%
 @for /L %%i in (1,1,10) do @(
 @echo fold %%i
-@%theexe% %datafold%\adult_cv%%i       -u
-@%theexe% %datafold%\car_cv%%i         -u
-@%theexe% %datafold%\isolet_cv%%i      -u
-@%theexe% %datafold%\page-blocks_cv%%i -u
-@%theexe% %datafold%\winequality_cv%%i -u
+@%theexe% %data%_cv%%i -u
+)
+
+@set data=%datafolder%\car
+@echo %data%
+@for /L %%i in (1,1,10) do @(
+@echo fold %%i
+@%theexe% %data%_cv%%i -u
+)
+
+@set data=%datafolder%\isolet
+@echo %data%
+@for /L %%i in (1,1,10) do @(
+@echo fold %%i
+@%theexe% %data%_cv%%i -u
+)
+
+@set data=%datafolder%\page-blocks
+@echo %data%
+@for /L %%i in (1,1,10) do @(
+@echo fold %%i
+@%theexe% %data%_cv%%i -u
+)
+
+@set data=%datafolder%\winequality
+@echo %data%
+@for /L %%i in (1,1,10) do @(
+@echo fold %%i
+@%theexe% %data%_cv%%i -u
 )
 
 :END
