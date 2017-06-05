3cba43d96b94e56703cf4bd0a71f7c74893c9822
aaaaagold <alexhhxela@gmail.com>
2017-06-05 11:09:22 +0800

    Update make.bat

--- ai2017/hw2/make.bat
+++ ai2017/hw2/make.bat
@@ -1,3 +1,3 @@
 
-g++ -std=c++11 -O2 -s cv-main.cpp -o cv-main.cpp.exe
-g++ -std=c++11 -O2 -s cv-main.cpp -o nb-main.cpp.exe
+g++ -std=c++11 -O2 cv-main.cpp -o cv-main.cpp.exe
+g++ -std=c++11 -O2 nb-main.cpp -o nb-main.cpp.exe
