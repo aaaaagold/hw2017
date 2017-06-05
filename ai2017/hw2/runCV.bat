dd6be7d37a121e3bfe4f3b8efdf4349a15ec4296
aaaaagold <alexhhxela@gmail.com>
2017-06-05 20:43:16 +0800

    Update runCV.bat

--- ai2017/hw2/runCV.bat
+++ ai2017/hw2/runCV.bat
@@ -1,13 +1,13 @@
 
 @set CVPath=cv-main.cpp.exe
-@set datafold="dataset"
+@set datafolder="dataset"
 
 @echo cutting data
 @set theexe=%CVPath%
-@%theexe% %datafold%\adult
-@%theexe% %datafold%\car
-@%theexe% %datafold%\isolet
-@%theexe% %datafold%\page-blocks
-@%theexe% %datafold%\winequality
+@%theexe% %datafolder%\adult
+@%theexe% %datafolder%\car
+@%theexe% %datafolder%\isolet
+@%theexe% %datafolder%\page-blocks
+@%theexe% %datafolder%\winequality
 
 :END
